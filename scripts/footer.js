aa170082894c7320c691f8842275f9db3444d751
Erangamadhushan <erangamadhushann956@gmail.com>
2025-01-17 00:59:01 +0530

    Refactor footer content structure and implement main content rendering

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,8 +1,38 @@
-import { navbarContent } from './contents/footerContent.js';
+import { footerContent } from './contents/footerContent.js';
 window.addEventListener("DOMContentLoaded", () => {
-    renderFooter(navbarContent);
+    renderFooter(footerContent);
 });
 
 function renderFooter(contents) {
+    const footerContainer = document.querySelector(".footer");
+    let footerContent = `
+        <div class="row">
+            <div class="col-sm-3 d-flex flex-col text-center">
+                <p><a href="#" class=" ">${contents[0].title}</a></p>
+                <p><a href="#" class="">${contents[0].link1}</a></p>
+                <p><a href="#" class="">${contents[0].link2}</a></p>
+                <p><a href="#" class="">${contents[0].link3}</a></p>
+            </div>
+            <div class="col-sm-3 text-center">
+                <p><a href="#" class="">${contents[1].title}</a></p>
+                <p><a href="#" class="">${contents[1].link1}</a></p>
+                <p><a href="#" class="">${contents[1].link2}</a></p>
+                <p><a href="#" class="">${contents[1].link3}</a></p>
+            </div>
+            <div class="col-sm-3 text-center">
+                <p><a href="#" class="">${contents[2].title}</a></p>
+                <p><a href="#" class="">${contents[2].link1}</a></p>
+                <p><a href="#" class="">${contents[2].link2}</a></p>
+                <p><a href="#" class="">${contents[2].link3}</a></p>
+            </div>
+            <div class="col-sm-3 text-center">
+                <p><a href="#" class="">${contents[3].title}</a></p>
+                <p><a href="#" class="">${contents[3].link1}</a></p>
+                <p><a href="#" class="">${contents[3].link2}</a></p>
+                <p><a href="#" class="">${contents[3].link3}</a></p>
+            </div>
+        </div>
+    `;
 
+    footerContainer.innerHTML = footerContent;
 }
---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", () => {
+    renderMainContent();
+});
+
+function renderMainContent() {
+    const mainContainer = document.querySelector("#root");\
+    let rootContent = `
+        <div class="">
+        </div>
+    `;
+}
