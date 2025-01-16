49134b1d475bf39120878896b314a5e96eea19cb
Erangamadhushan <erangamadhushann956@gmail.com>
2025-01-17 01:13:58 +0530

    Enhance footer layout and add main content styling for improved presentation

--- scripts/footer.js
+++ scripts/footer.js
@@ -7,29 +7,34 @@ function renderFooter(contents) {
     const footerContainer = document.querySelector(".footer");
     let footerContent = `
         <div class="row">
-            <div class="col-sm-3 d-flex flex-col text-center">
+            <div class="col-md-3 col-sm-5 d-flex flex-col text-center my-5">
                 <p><a href="#" class=" ">${contents[0].title}</a></p>
                 <p><a href="#" class="">${contents[0].link1}</a></p>
                 <p><a href="#" class="">${contents[0].link2}</a></p>
                 <p><a href="#" class="">${contents[0].link3}</a></p>
             </div>
-            <div class="col-sm-3 text-center">
+            <div class="col-md-3 col-sm-5 text-center my-5">
                 <p><a href="#" class="">${contents[1].title}</a></p>
                 <p><a href="#" class="">${contents[1].link1}</a></p>
                 <p><a href="#" class="">${contents[1].link2}</a></p>
                 <p><a href="#" class="">${contents[1].link3}</a></p>
             </div>
-            <div class="col-sm-3 text-center">
+            <div class="col-md-3 col-sm-5 text-center my-5">
                 <p><a href="#" class="">${contents[2].title}</a></p>
                 <p><a href="#" class="">${contents[2].link1}</a></p>
                 <p><a href="#" class="">${contents[2].link2}</a></p>
                 <p><a href="#" class="">${contents[2].link3}</a></p>
             </div>
-            <div class="col-sm-3 text-center">
+            <div class="col-md-3 col-sm-5 text-center my-5">
                 <p><a href="#" class="">${contents[3].title}</a></p>
                 <p><a href="#" class="">${contents[3].link1}</a></p>
                 <p><a href="#" class="">${contents[3].link2}</a></p>
                 <p><a href="#" class="">${contents[3].link3}</a></p>
+            </div>
+        </div>
+        <div class="row bg-dark">
+            <div>
+                <h3 class="text-white text-center">EM956 Community</h3>
             </div>
         </div>
     `;
